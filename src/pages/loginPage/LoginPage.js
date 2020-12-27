019fb68b40ab2b74d4a62a58a5ed441bf2afea7f
Nitin naik <GNitin123>
2020-12-28 01:33:21 +0530

    Add->Added validations

--- src/pages/loginPage/LoginPage.js
+++ src/pages/loginPage/LoginPage.js
@@ -4,38 +4,56 @@ import loginImage from './images/account.svg'
 function LoginPage(props) {
     return (
         <div>
-             <div className='login-container'>
-                    <div className="form-login-heading">
-                    <p>Login to your account</p>
-                    </div>
-                    <div className='form-container'>
-                        <form className='login-form' onSubmit={(event) =>props.handleLoginFormSubmit(event)}>
-                            <div className='input-box-position'>
+            <div className='login-container'>
+                <div className="login-form-heading">
+                    <div>Login to your account</div>
+                </div>
+                <div className='form-container'>
+                    <form
+                        className='login-form'
+                        onSubmit={event => props.handleLoginFormSubmit(event)}
+                        autoComplete="off">
+                        <div className='input-box-position'>
                             <div className='userInput-box'>
-                            <input 
-                            type='text' 
-                            className='login-input'
-                            placeholder='Username'/>
+                                <input
+                                    type='text'
+                                    name='userName'
+                                    placeholder=' '
+                                    value={props.stateData.user.userName}
+                                    className='login-input'
+                                    onChange={event => props.handleInputChange(event)}
+                                    onInput={props.handleUserErrorMessage}/>
+                                <label className='username-label'>Username</label>
                             </div>
-                            <span className='form-error-message'>{props.stateData.userName}</span>
-                            </div>
-                            <div className='password-box'>
-                            <input 
-                            type='password' 
-                            className='login-password'
-                            placeholder='Password'/>
-                            </div>
-                            <span className='form-error-message'>{props.stateData.password}</span>
-                            <button className='login-button'>Login</button>
-                            <p className='forgot-password-link'>Forgot password?</p>
-                        </form>
-                        <p className='create-account-link'>Create Account</p>
-                    </div>
-                    <div className='image-position'>
+                            <span className='username-error-message'>{props.stateData.userNameErrorMessage}</span>
+                        </div>
+                        <div className='password-box'>
+                            <input
+                                className='login-password'
+                                type={props.stateData.passwordVisibility?'password':'text'}
+                                name='password'
+                                placeholder=' '
+                                value={props.stateData.user.password}
+                                onChange={event => props.handleInputChange(event)}
+                                onInput={props.handlePasswordErrorMessage}/>
+                            <label className='password-label'>Password</label>
+                            <i
+                                className={props.stateData.passwordVisibility
+                                ? "fas fa-eye-slash"
+                                : "far fa-eye"}
+                                onClick={props.changePasswordVisibilityType}></i>
+                        </div>
+                        <span className='password-error-message'>{props.stateData.passwordErrorMessage}</span>
+                        <button type="submit" className='login-button'>Login</button>
+                        <div className='forgot-password-link'>Forgot password?</div>
+                    </form>
+                    <div className='new-account-link'>Create Account</div>
+                </div>
+                <div className='image-position'>
                     <figure>
                         <img src={loginImage} alt='Login-image'/>
                     </figure>
-                    </div>
+                </div>
             </div>
         </div>
     )
